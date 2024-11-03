6d81e1fa83550f31328556b297cc6944f2d33620
bensatlantik <bensantora@proton.me>
2024-11-03 06:26:02 -0500

    Add README.md

--- target/package/crabi_test-0.1.2/target/debug/deps/crabi_test-48809598c2042c25.d
+++
@@ -1,7 +0,0 @@
-/home/ben/rust_libs/crabi_test/target/package/crabi_test-0.1.2/target/debug/deps/libcrabi_test-48809598c2042c25.rmeta: src/lib.rs
-
-/home/ben/rust_libs/crabi_test/target/package/crabi_test-0.1.2/target/debug/deps/libcrabi_test-48809598c2042c25.rlib: src/lib.rs
-
-/home/ben/rust_libs/crabi_test/target/package/crabi_test-0.1.2/target/debug/deps/crabi_test-48809598c2042c25.d: src/lib.rs
-
-src/lib.rs:
