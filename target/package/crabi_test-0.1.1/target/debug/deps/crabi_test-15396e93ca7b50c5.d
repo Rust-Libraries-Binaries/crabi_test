e641cb0298a1c9314855e35923d9baae53cfaead
bensatlantik <bensantora@proton.me>
2024-11-03 06:11:37 -0500

    Fixed directory structure

---
+++ target/package/crabi_test-0.1.1/target/debug/deps/crabi_test-15396e93ca7b50c5.d
@@ -0,0 +1,7 @@
+/home/ben/rust_libs/crabi_test/target/package/crabi_test-0.1.1/target/debug/deps/libcrabi_test-15396e93ca7b50c5.rmeta: src/lib.rs
+
+/home/ben/rust_libs/crabi_test/target/package/crabi_test-0.1.1/target/debug/deps/libcrabi_test-15396e93ca7b50c5.rlib: src/lib.rs
+
+/home/ben/rust_libs/crabi_test/target/package/crabi_test-0.1.1/target/debug/deps/crabi_test-15396e93ca7b50c5.d: src/lib.rs
+
+src/lib.rs:
